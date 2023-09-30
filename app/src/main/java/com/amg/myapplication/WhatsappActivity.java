67fc62394e42e336c0ec311b02f8792af1a2ff88
mokwadigibe <mokwadiamg@gmail.com>
2023-09-30 21:39:51 +0200

    Added whatsapp fragments

---
+++ app/src/main/java/com/amg/myapplication/WhatsappActivity.java
@@ -0,0 +1,14 @@
+package com.amg.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhatsappActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whatsapp);
+    }
+}
